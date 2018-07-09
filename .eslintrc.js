f376baf05d8f987d0f031bac70c6c918eee5cbf1
Lu-c-s <Lucas3645@gmail.com>
2018-07-09 01:12:50 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    parserOptions: {
+        parser: 'babel-eslint',
+        "ecmaVersion": 6,
+      },
+};
---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+
+class App extends Component {
+  state = {
+    persons: [
+      {
+        id : '1',
+        name: 'Max',
+        age: 28
+      },
+      {
+        id : '2',
+        name: 'Ke',
+        age: 35
+      },
+      {
+        id : '3',
+        name: 'Kelly',
+        age: 39
+      },
+      {
+        id : '4',
+        name: 'Nana',
+        age: 17
+      }
+    ],
+   showPersons : false
+  }
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    //or
+    // const person = Object.assign({}, this.state.persons[personIndex])
+    
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons : persons})
+  }
+  togglePersonsHandler = () => {
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons : !doesShow});
+  }
+  deletePersonHandler = (personIndex) => {
+   //const persons = this.state.persons.slice();
+   //or
+   const persons = [...this.state.persons]; 
+   persons.splice(personIndex,1);
+
+    this.setState({persons: persons});
+  }
+
+  render() {
+    let persons  = null;
+    
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+
+    if(this.state.showPersons) {
+      persons = (
+        <div> 
+          {this.state.persons.map((person, index) => {
+            return <Person
+                   click={() => this.deletePersonHandler(index)}
+                   name={person.name} 
+                   age={person.age}
+                   key={person.id}
+                   changed={(event) => this.nameChangeHandler(event, person.id)}/>
+          })}
+        </div>
+      );
+      style.backgroundColor = 'red';
+   
+    }
+    
+    const classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+
+    return (
+      <div className="App">
+        <p className={classes.join(' ')}> Hi i'm a react App </p>
+        <button onClick={this.togglePersonsHandler} style={style}>Switch Name</button>
+        
+        {persons}
+        
+      </div>
+    );
+  }
+}
+
+export default Radium(App);
